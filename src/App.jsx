d23f42283b8148830112a0739b4195d60f1c3251
Nitzan Maizel <nitzan.maizel@zoominsoftware.com>
2022-02-08 22:43:47 +0200

    create board and square scss files

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Board from './components/Board/Board';
 
+import './styles/root.scss';
+
 const App = () => {
   return (
-    <>
+    <div className="app">
       <h1>TIC TAC TOE</h1>
       <Board />
-    </>
+    </div>
   );
 };
 
--- src/components/Square/Square.jsx
+++ src/components/Square/Square.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
 
+import './Square.scss';
+
 const Square = ({ value }) => {
-  return <button type="button">{value}</button>;
+  return (
+    <button type="button" className="square">
+      {value}
+    </button>
+  );
 };
 
 export default Square;
