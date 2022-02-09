d9dfcd9f15ab5fce2e5f2127e2bf6b70bed72924
Nitzan Maizel <nitzan.maizel@zoominsoftware.com>
2022-02-09 02:13:33 +0200

    Board - create renderBoard fun and change Square styles

--- src/components/Board/Board.jsx
+++ src/components/Board/Board.jsx
@@ -32,25 +32,11 @@ const Board = () => {
     );
   };
 
-  return (
-    <div className="board">
-      <div className="board-row">
-        {renderSquare(0)}
-        {renderSquare(1)}
-        {renderSquare(2)}
-      </div>
-      <div className="board-row">
-        {renderSquare(3)}
-        {renderSquare(4)}
-        {renderSquare(5)}
-      </div>
-      <div className="board-row">
-        {renderSquare(6)}
-        {renderSquare(7)}
-        {renderSquare(8)}
-      </div>
-    </div>
-  );
+  const renderBoard = () => {
+    return board.map((value, index) => renderSquare(index));
+  };
+
+  return <div className="board">{renderBoard()}</div>;
 };
 
 export default Board;
