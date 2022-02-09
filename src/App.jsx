c18e90fcf0a6bd30ca2034d8f1c6ba29bb2e6831
Nitzan Maizel <nitzan.maizel@zoominsoftware.com>
2022-02-09 02:23:26 +0200

    App - move logic and state from board component to app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Board from './components/Board/Board';
 
 import './styles/root.scss';
 
 const App = () => {
+  const [board, setBoard] = useState(Array(9).fill(null));
+  const [isXNext, setIsXNext] = useState(true);
+
+  const handleSquareClick = position => {
+    if (board[position]) return;
+
+    setBoard(preBoardValue => {
+      return preBoardValue.map((square, squarePosition) => {
+        if (squarePosition === position) {
+          return isXNext ? 'X' : '0';
+        }
+
+        return square;
+      });
+    });
+
+    setIsXNext(!isXNext);
+  };
+
   return (
     <div className="app">
       <h1>TIC TAC TOE</h1>
-      <Board />
+      <Board boardState={board} isXNext={isXNext} onClick={handleSquareClick} />
     </div>
   );
 };
--- src/components/Board/Board.jsx
+++ src/components/Board/Board.jsx
@@ -1,42 +1,24 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Square from '../Square/Square';
 
 import './Board.scss';
 
-const Board = () => {
-  const [board, setBoard] = useState(Array(9).fill(null));
-  const [isXNext, setIsXNext] = useState(true);
-
-  const handleSquareClick = position => {
-    if (board[position]) return;
-
-    setBoard(preBoardValue => {
-      return preBoardValue.map((square, squarePosition) => {
-        if (squarePosition === position) {
-          return isXNext ? 'X' : '0';
-        }
-
-        return square;
-      });
-    });
-
-    setIsXNext(!isXNext);
-  };
-
-  const renderSquare = position => {
+const Board = ({ boardState, onClick }) => {
+  const renderSquare = (squareValue, position) => {
     return (
       <Square
-        value={board[position]}
-        onClick={() => handleSquareClick(position)}
+        key={position}
+        value={squareValue}
+        onClick={() => onClick(position)}
       />
     );
   };
 
-  const renderBoard = () => {
-    return board.map((value, index) => renderSquare(index));
+  const renderBoard = boardState => {
+    return boardState.map((value, index) => renderSquare(value, index));
   };
 
-  return <div className="board">{renderBoard()}</div>;
+  return <div className="board">{renderBoard(boardState)}</div>;
 };
 
 export default Board;
