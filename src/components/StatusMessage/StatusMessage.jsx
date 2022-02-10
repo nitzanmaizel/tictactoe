50477fe1bccb5d3cf93509be985139cdc01217ae
Nitzan Maizel <nitzan.maizel@zoominsoftware.com>
2022-02-11 01:02:45 +0200

    StatusMessage - chaneg render text and add text class

--- src/components/StatusMessage/StatusMessage.jsx
+++ src/components/StatusMessage/StatusMessage.jsx
@@ -1,16 +1,36 @@
 import React from 'react';
 
+import './StatusMessage.scss';
+
 const StatusMessage = ({ winner, currentGame }) => {
   const noMovesLeft = currentGame.board.every(move => move !== null);
 
   return (
-    <h2>
-      {winner && `Winner is ${winner}`}
-      {!winner &&
-        !noMovesLeft &&
-        `Next Player Is ${currentGame.isXNext ? 'X' : '0'}`}
-      {!winner && noMovesLeft && 'X and O tied'}
-    </h2>
+    <div className="status-message">
+      {winner && (
+        <>
+          <span>Winner is </span>
+          <span className={winner === 'X' ? 'text-green' : 'text-orange'}>
+            {winner}
+          </span>
+        </>
+      )}
+      {!winner && !noMovesLeft && (
+        <>
+          <span>Next player is </span>
+          <span className={currentGame.isXNext ? 'text-green' : 'text-orange'}>
+            {currentGame.isXNext ? 'X' : 'O'}{' '}
+          </span>
+        </>
+      )}
+      {!winner && noMovesLeft && (
+        <>
+          <span className="text-green">X </span>
+          <span>and </span>
+          <span className="text-orange">O</span> tied
+        </>
+      )}
+    </div>
   );
 };
 
