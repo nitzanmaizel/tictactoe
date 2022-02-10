cb44af7b416b4bda823107a6d625c9758dedb421
Nitzan Maizel <nitzan.maizel@zoominsoftware.com>
2022-02-10 22:27:17 +0200

    App - transform board to state that keep game history

--- src/App.jsx
+++ src/App.jsx
@@ -5,35 +5,47 @@ import './styles/root.scss';
 import { calculateWinner } from './utils/CalculateWinner';
 
 const App = () => {
-  const [board, setBoard] = useState(Array(9).fill(null));
-  const [isXNext, setIsXNext] = useState(true);
+  const [history, setHistory] = useState([
+    {
+      board: Array(9).fill(null),
+      isXNext: true,
+    },
+  ]);
 
-  const winner = calculateWinner(board);
+  const [currentMove, setCurrentMove] = useState(0);
+
+  const current = history[currentMove];
+
+  const winner = calculateWinner(current.board);
   const massage = winner
     ? `Winner is ${winner}`
-    : `Next Player Is ${isXNext ? 'X' : '0'}`;
+    : `Next Player Is ${current.isXNext ? 'X' : '0'}`;
 
   const handleSquareClick = position => {
-    if (board[position] || winner) return;
+    if (current.board[position] || winner) return;
 
-    setBoard(preBoardValue => {
-      return preBoardValue.map((square, squarePosition) => {
+    setHistory(preBoardValue => {
+      const last = preBoardValue[preBoardValue.length - 1];
+
+      const newBoard = last.board.map((square, squarePosition) => {
         if (squarePosition === position) {
-          return isXNext ? 'X' : 'O';
+          return last.isXNext ? 'X' : 'O';
         }
 
         return square;
       });
+
+      return preBoardValue.concat({ board: newBoard, isXNext: !last.isXNext });
     });
 
-    setIsXNext(!isXNext);
+    setCurrentMove(currentMove + 1);
   };
 
   return (
     <div className="app">
       <h1>TIC TAC TOE</h1>
       <h2>{massage}</h2>
-      <Board boardState={board} onClick={handleSquareClick} />
+      <Board boardState={current.board} onClick={handleSquareClick} />
     </div>
   );
 };
