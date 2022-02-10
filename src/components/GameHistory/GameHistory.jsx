31a5f9ff02c55b79cb42eb04539252fe9a82fae1
Nitzan Maizel <nitzan.maizel@zoominsoftware.com>
2022-02-10 22:41:00 +0200

    GameHistory - active moveToMoveNumber fun on click

--- src/components/GameHistory/GameHistory.jsx
+++ src/components/GameHistory/GameHistory.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 
-const GameHistory = ({ gameHistory }) => {
+const GameHistory = ({ gameHistory, moveToMoveNumber }) => {
   return (
     <ul>
-      {gameHistory.map((_, move) => (
-        <li key={move}>
-          <button>
-            {move === 0 ? 'Go to game start' : `Go to move number ${move}`}
+      {gameHistory.map((_, moveNumber) => (
+        <li key={moveNumber}>
+          <button onClick={() => moveToMoveNumber(moveNumber)}>
+            {moveNumber === 0
+              ? 'Go to game start'
+              : `Go to move number ${moveNumber}`}
           </button>
         </li>
       ))}
