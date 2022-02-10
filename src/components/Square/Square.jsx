fa4ff85ed9a3b423c28344349226d1c181d0f300
Nitzan Maizel <nitzan.maizel@zoominsoftware.com>
2022-02-11 01:12:19 +0200

    Square - add diff text-class to X and O

--- src/components/Square/Square.jsx
+++ src/components/Square/Square.jsx
@@ -6,7 +6,9 @@ const Square = ({ value, onClick, isWinningSquare }) => {
   return (
     <button
       type="button"
-      className={`square ${isWinningSquare ? 'winning' : ''}`}
+      className={`square ${isWinningSquare ? 'winning' : ''} ${
+        value === 'X' ? 'text-green' : 'text-orange'
+      }`}
       onClick={onClick}
     >
       {value}
