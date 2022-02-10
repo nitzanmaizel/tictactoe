c0c7fdeb316272d932ebc8311977a79781a2f29f
Nitzan Maizel <nitzan.maizel@zoominsoftware.com>
2022-02-11 00:35:31 +0200

    GameHistory - add diff class

--- src/components/GameHistory/GameHistory.jsx
+++ src/components/GameHistory/GameHistory.jsx
@@ -4,20 +4,24 @@ import './GameHistory.scss';
 
 const GameHistory = ({ gameHistory, moveToMoveNumber, currentMove }) => {
   return (
-    <ul>
-      {gameHistory.map((_, moveNumber) => (
-        <li key={moveNumber}>
-          <button
-            className={currentMove === moveNumber && 'activeButton'}
-            onClick={() => moveToMoveNumber(moveNumber)}
-          >
-            {moveNumber === 0
-              ? 'Go to game start'
-              : `Go to move number ${moveNumber}`}
-          </button>
-        </li>
-      ))}
-    </ul>
+    <div className="GameHistory-wrapper">
+      <ul className="GameHistory">
+        {gameHistory.map((_, moveNumber) => (
+          <li key={moveNumber}>
+            <button
+              className={`btn-move ${
+                currentMove === moveNumber ? 'activeButton' : ''
+              }`}
+              onClick={() => moveToMoveNumber(moveNumber)}
+            >
+              {moveNumber === 0
+                ? 'Go to game start'
+                : `Go to move number ${moveNumber}`}
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 };
 
